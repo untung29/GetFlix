b08059b8297cf26b7a3eb1e57e59d503b3a05c22
Untung <u.untung0029@gmail.com>
2022-04-02 13:30:13 +1100

    Navigation to search result page and set the query param to have title

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -5,7 +5,7 @@ const Logo = styled.h1`
   font-size: 3.5rem;
   margin-left: 1rem;
   margin-top: 0.5rem;
-  font-family: 'Helvetica' sans-serif;
+  font-family: 'Helvetica';
   position: absolute;
   @media (max-width: 768px) {
     font-size: 3rem;
--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -1,25 +1,45 @@
 import { IconButton, TextField } from '@mui/material';
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
+import { useNavigate } from 'react-router-dom';
 
-const TextInput = ({ label, isLanding }: { label: string; isLanding: boolean }): ReactElement => (
-  <TextField
-    label={label}
-    variant="filled"
-    style={{
-      backgroundColor: 'white',
-      borderRadius: '1rem',
-    }}
-    fullWidth={isLanding}
-    InputProps={{
-      disableUnderline: true,
-      endAdornment: (
-        <IconButton onClick={() => {}}>
-          <SearchIcon />
-        </IconButton>
-      ),
-    }}
-  />
-);
+const TextInput = ({ label, isLanding }: { label: string; isLanding: boolean }): ReactElement => {
+  const navigate = useNavigate();
+  const [title, setTitle] = useState('');
+
+  const onSetTitle = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setTitle(e.target.value);
+  };
+
+  const onSearchTitle = () => {
+    navigate({ pathname: '/search', search: `?t=${title}` });
+  };
+
+  return (
+    <TextField
+      onChange={onSetTitle}
+      label={label}
+      variant="filled"
+      style={{
+        backgroundColor: 'white',
+        borderRadius: '1rem',
+      }}
+      fullWidth={isLanding}
+      InputProps={{
+        disableUnderline: true,
+        endAdornment: (
+          <IconButton onClick={onSearchTitle}>
+            <SearchIcon />
+          </IconButton>
+        ),
+      }}
+      onKeyPress={(e): void => {
+        if (e.key === 'Enter') {
+          onSearchTitle();
+        }
+      }}
+    />
+  );
+};
 
 export default TextInput;
