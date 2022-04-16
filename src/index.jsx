5bdf9016899e3998e4b70bc37f19ccb7d2a04fdb
Untung <u.untung0029@gmail.com>
2022-04-16 15:49:24 +1000

    add import css

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,7 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+// eslint-disable-next-line no-unused-vars
+import css from 'index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
